175dffeb3a933a56429aa57e81e3f207a1098a5a
Eskola Antti <antti.eskola@almamedia.fi>
2021-04-29 10:16:47 +0300

    mockdata

--- context/mockData.ts
+++ context/mockData.ts
@@ -16,4 +16,59 @@ export const myCoins =
         name:'Ethereum',
         coinPriceOnPurchace: 385.8445887975079,
         coinAmount: 25.917170514598098
-    }]
+    },
+    {
+        id: 'cardano',
+        purchaseDate: '01-11-2020',
+        purchaseAmount: 10000,
+        symbol: 'ada',
+        name:'Cardano',
+        coinPriceOnPurchace: 0.09286959263589264,
+        coinAmount: 107677.87083
+    },
+    {
+        id: 'dec',
+        purchaseDate: '01-11-2020',
+        purchaseAmount: 10000,
+        symbol: 'dec',
+        name:'Decentr',
+        coinPriceOnPurchace: 0.035324445335684004,
+        coinAmount: 283090.12117
+},
+{
+    id: 'tether',
+    purchaseDate: '01-11-2020',
+    purchaseAmount: 10000,
+    symbol: 'usdt',
+    name:'Tether',
+    coinPriceOnPurchace: 0.9977885,
+    coinAmount: 10022.1640
+},
+{
+    id: 'nexo',
+    purchaseDate: '01-11-2020',
+    purchaseAmount: 10000,
+    symbol: 'nexo',
+    name:'NEXO',
+    coinPriceOnPurchace: 0.176348,
+    coinAmount: 56706.05847
+},
+{
+    id: 'vechain',
+    purchaseDate: '01-11-2020',
+    purchaseAmount: 10000,
+    symbol: 'vet',
+    name:'VeChain',
+    coinPriceOnPurchace: 0.00987544,
+    coinAmount: 1012658.227848
+},
+{
+    id: 'binancecoin',
+    purchaseDate: '01-11-2020',
+    purchaseAmount: 10000,
+    symbol: 'bnb',
+    name:'Binance Coin',
+    coinPriceOnPurchace: 0.00987544,
+    coinAmount: 352.2888
+}
+]
